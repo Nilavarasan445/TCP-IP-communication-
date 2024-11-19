31c3ee12054c9f5b7d4cc9685c6d5855851a8499
Jana <janathiru2003@gmail.com>
2024-11-19 16:57:22 +0530

    Instead of running the server infintely cutonce all the clients are processed

--- tcp_server.py
+++ tcp_server.py
@@ -3,10 +3,12 @@ import threading
 from protocols import Protocol
 
 class TCPServer:
-    def __init__(self, host=socket.gethostname(), port=9999, protocol: Protocol = None):
+    def __init__(self, host=socket.gethostname(), port=9999, max_clients=5):
         self.host = host
         self.port = port
         self.server_socket = None
+        self.max_clients = max_clients
+        self.semaphore = threading.Semaphore(max_clients)  
 
     def protocol_handler(self, data, c_address):
         command = data.decode('utf-8').split(':')[0]
@@ -34,20 +36,22 @@ class TCPServer:
             print(f"Error handling client {c_address}: {e}")
         finally:
             client_socket.close()
+            self.semaphore.release()
 
     def start_server(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((self.host, self.port))
-        self.server_socket.listen(10)
-        print(f"Server started at {self.host}:{self.port}.")
+        self.server_socket.listen(self.max_clients)
+        print(f"Server started at {self.host}:{self.port} with a limit of {self.max_clients} clients.")
 
-        while True:
+        for _ in range(self.max_clients):
             client_socket, client_address = self.server_socket.accept()
             print(f"Connection from {client_address} established.")
             
+            self.semaphore.acquire()
             client_thread = threading.Thread(target=self.process_client, args=(client_socket, client_address))
             client_thread.start()
 
 if __name__ == '__main__':
-    server = TCPServer()
+    server = TCPServer(max_clients=5)
     server.start_server()
